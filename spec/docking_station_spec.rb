8f975cfd03ea0c9a61637b2ed92f069aaed7de67
Ben Stuart <benarthurstuart@hotmail.com>
2019-10-17 15:30:41 +0100

    step 19

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,38 +5,36 @@ describe DockingStation do
 
   describe '#dock' do
     it 'docks a bike' do
-      bike = Bike.new
-      subject.dock(bike)
+      bike = double(:bike)
+      subject.dock bike
       expect(subject.bikes[0]).to eq bike
     end
 
     it 'raises an error when full' do
-      DockingStation::DEFAULT_CAPACITY.times { subject.dock(Bike.new) }
-      expect { subject.dock(Bike.new) }.to raise_error 'docking station at capacity'
+      DockingStation::DEFAULT_CAPACITY.times { subject.dock(double(:bike)) }
+      expect { subject.dock double(:bike) }.to raise_error 'docking station at capacity'
     end
 
-
   end
-
 
   describe '#releases_bike' do
     it { is_expected.to respond_to :release_bike }
 
     it 'does not release broken bikes' do
-      bike = Bike.new
+      bike = double(:bike)
       bike.report_bike
       subject.dock(bike)
       expect { subject.release_bike }.to raise_error 'broken bike'
     end
 
     it "releases working bikes" do
-      bike = Bike.new
+      bike = double(:bike)
       subject.dock(bike)
       expect(subject.release_bike).to be_working
     end
 
     it 'releases a bike' do
-      bike = Bike.new
+      bike = double(:bike)
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
@@ -48,8 +46,8 @@ describe DockingStation do
 
   describe '#bikes' do
     it 'shows bikes in the docking station' do
-      bike1 = Bike.new
-      bike2 = Bike.new
+      bike1 = double(:bike)
+      bike2 = double(:bike)
       subject.dock(bike1)
       subject.dock(bike2)
       expect(subject.bikes).to eq [bike1, bike2]
